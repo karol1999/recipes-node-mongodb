5ffb0bcf059d485ccd45a382be8de14d496d4575
Dariusz Nowak <nowakdariusz03@gmail.com>
2021-02-16 17:31:08 +0100

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-const recipesRouter = require('./recipes/recipes.controller')
-const usersRouter = require('./users/users.controller')
-const router = new Router();
-
-router.use('/recipes', recipesRouter);
-router.use('/users', usersRouter);
-
-module.exports = router;
