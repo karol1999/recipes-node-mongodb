e4b0d44d1b821bd75848ee72eeec5a83069c5e9e
Dariusz Nowak <nowakdariusz03@gmail.com>
2021-02-16 17:30:56 +0100

    Delete exceptions directory

--- exceptions/user-not-found.exception.js
+++
@@ -1,8 +0,0 @@
-class UserNotFoundException extends Error {
-    constructor(message) {
-        super(message || "User not found");
-        this.status = 404
-    }
-}
-
-module.exports = UserNotFoundException;
